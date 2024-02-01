6952be0e01cce49781ce04272adbce6fb4e0b8c5
Randomsnowman2005 <mohdali.fakhoury@gmail.com>
2024-02-01 14:10:52 -0600

    Prob 1 done

--- Problem 1/right_arrow.py
+++ Problem 1/right_arrow.py
@@ -3,13 +3,16 @@ def right_arrow():
     head_char = input()
 
     row1 = '      ' + head_char
-    ''' Type your code here. '''
+    Base = base_char + base_char +base_char+base_char+base_char
+    row2 = Base+ head_char + head_char
+    row3 = Base + head_char+head_char+head_char
+    row4 = Base + head_char
 
     print(row1)
     print(row2)
     print(row3)
     print(row2)
-    print(row1)
+    print(row4)
 
 if __name__ == "__main__":
     right_arrow()
