feb0b9e9b278faa444352d0ede3e6d0b29b4c344
Randomsnowman2005 <mohdali.fakhoury@gmail.com>
2024-02-01 15:01:56 -0600

    basic problem 2- needs formatting

--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -1,6 +1,9 @@
 def telephone():
     phone_number = int(input())
-    Strt = phone_number // 10000000
-    print( '('Strt')' )
+    Start = phone_number // 10000000
+    cut = phone_number % 10000000
+    mid = cut // 10000
+    end = cut % 10000
+    print( '(',Start,')', mid,'-',end)
 if __name__ == "__main__":
     telephone()
