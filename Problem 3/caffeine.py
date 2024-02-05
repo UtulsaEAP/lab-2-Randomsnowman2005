760a91a03f46dde9db5fc281f2f9c328b1571cce
Randomsnowman2005 <mohdali.fakhoury@gmail.com>
2024-02-05 14:58:12 -0600

    prob 3 small change,  the github test is not in version 3.11 as it did not recognise math.exp2, updated code to fit older version

--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -2,9 +2,9 @@
 import math
 def caffeine():
     caffeine_mg = float(input())
-    print (f'After 6 hours: {caffeine_mg / math.exp2(1):.2f} mg')
-    print (f'After 12 hours: {caffeine_mg / math.exp2(2):.2f} mg')
-    print (f'After 24 hours: {caffeine_mg / math.exp2(4):.2f} mg')
+    print (f'After 6 hours: {caffeine_mg / math.pow(2,1):.2f} mg')
+    print (f'After 12 hours: {caffeine_mg / math.pow(2,2):.2f} mg')
+    print (f'After 24 hours: {caffeine_mg / math.pow(2,4):.2f} mg')
     
 if __name__ == "__main__":
     caffeine()
